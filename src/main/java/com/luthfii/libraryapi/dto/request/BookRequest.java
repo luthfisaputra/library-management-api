c3675f5cf52da33e84d2d1d278b4e024dfe2fb72
luthfisaputra <luthfsptr@gmail.com>
2026-06-19 18:44:11 +0700

    add message

--- src/main/java/com/luthfii/libraryapi/dto/request/BookRequest.java
+++ src/main/java/com/luthfii/libraryapi/dto/request/BookRequest.java
@@ -1,5 +1,6 @@
 package com.luthfii.libraryapi.dto.request;
 
+import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
@@ -16,13 +17,15 @@ public class BookRequest {
     @NotNull(message = "Judul tidak boleh kosong")
     private String title;
 
-    @NotNull(message = "ISBN tidak boleh kosong")
+    @NotNull(message = "ISBN tidak boleh kosong & format 10 atau 13 digit")
     private String isbn;
 
     @NotNull(message = "Author ID tidak boleh kosong")
     private Long authorId;
 
     @NotNull(message = "Tahun terbit tidak boleh kosong")
+    @Min(value = 1000, message = "Tahun tidak valid")
+    @Max(value = 3000, message = "Tahun tidak valid")
     private Integer yearPublished;
 
     @NotNull(message = "Stok tidak boleh kosong")
